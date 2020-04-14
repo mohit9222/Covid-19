8d35fc94a6168d4660cd9e5e32e17d02a2d1ad34
mohit9222 <mohit.jan92@gmail.com>
2020-04-15 01:02:55 +0530

    Updates

---
+++ precache-manifest.88b9d3f658027ef8f6423269bb3dc6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40666a22e936b042a17f9553f96b09d4",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "76a6c06be593c118f257",
+    "url": "/Covid-19/static/css/main.d6794bc8.chunk.css"
+  },
+  {
+    "revision": "98ebb152797b93d0bf7a",
+    "url": "/Covid-19/static/js/2.bc47409e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.bc47409e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a6c06be593c118f257",
+    "url": "/Covid-19/static/js/main.8172c6a9.chunk.js"
+  },
+  {
+    "revision": "4461921d2eb3083ad21a",
+    "url": "/Covid-19/static/js/runtime-main.f37b298f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  }
+]);
