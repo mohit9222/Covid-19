daf1950578789d6bf9e5cac6e4caadd2dbed9f67
mohit9222 <mohit.jan92@gmail.com>
2020-04-29 15:58:10 +0530

    Updates

---
+++ precache-manifest.fde5f4f71348ac2de318b4378d2a75f8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219baa3df5f8158859562f4840b1836a",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "7232fbc025ffa868087d",
+    "url": "/Covid-19/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7ce87c40fc71ae16f6bf",
+    "url": "/Covid-19/static/css/main.8c7732b5.chunk.css"
+  },
+  {
+    "revision": "7232fbc025ffa868087d",
+    "url": "/Covid-19/static/js/2.11eb29bb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.11eb29bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce87c40fc71ae16f6bf",
+    "url": "/Covid-19/static/js/main.f8fe6527.chunk.js"
+  },
+  {
+    "revision": "4461921d2eb3083ad21a",
+    "url": "/Covid-19/static/js/runtime-main.f37b298f.js"
+  },
+  {
+    "revision": "8e54fe855c5f10f8bb0de2c99a167130",
+    "url": "/Covid-19/static/media/Protective.8e54fe85.PNG"
+  },
+  {
+    "revision": "ca2b460a6520381a4775e73cd566ddc6",
+    "url": "/Covid-19/static/media/covid.ca2b460a.jpg"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "25754ec597de4772adcd8599e74cdcdd",
+    "url": "/Covid-19/static/media/imp.25754ec5.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cfdb0dcb0b140f485445df7d56d6c7e5.js"
+  "/Covid-19/precache-manifest.fde5f4f71348ac2de318b4378d2a75f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Covid-19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
